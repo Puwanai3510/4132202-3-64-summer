ef91a750d67538b008f9ac350ae9fe8de92bf2e9
Puwanai Jaidee 8053 <94102983+Puwanai3510@users.noreply.github.com>
2022-04-22 13:21:18 +0700

    lab4

---
+++ lab4/www/php/profile.php
@@ -0,0 +1,9 @@
+<?php
+
+echo"Mr.Puwanai Jaidee <br>";
+echo"ID : 630112418053 <br>";
+echo"Nickname : โปรเต้ <br>";
+echo"Address : [บุรีรัมยฺ์] <br>";
+echo"Favourite : เกม,อนิเมะ,ฟังเพลง <br>";
+echo"Sport : ปีนเขา,ปลูกต้นไม้,ว่ายน้ำ,ยิงปืน <br>";
+echo"dream career : Animetor,";
